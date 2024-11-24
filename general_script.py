4e27308c2df1aca83a6ee8c184b3d85ff543261f
JP-Amboage <juangamboage@gmail.com>
2024-11-24 16:22:16 +0100

    add earth_distance attr to real graph + compute speed + min spanning tree

--- general_script.py
+++ general_script.py
@@ -7,13 +7,14 @@ import networkx as nx
 import numpy as np
 from ripser import ripser
 from persim import plot_diagrams
+import persim
 
-from utils import nx_to_pyg_data
-from utils import MyOwnDataset
+
 from utils import compute_persistence_statistics
 from utils import compute_ideal_matrix
-
-import persim
+from utils import add_earth_distnace
+from utils import compute_speed
+from utils import compute_ideal_graph
 
 def main(gtfs_path: str, out_path: str):
     
@@ -24,19 +25,22 @@ def main(gtfs_path: str, out_path: str):
     start = 00*60*60  # 7:00 AM
     end = 24*60*60
     G = pt.load_feed_as_graph(feed, start, end)
-
-    print(f"Saving graph to {out_path}")
+    
+    print(f"Adding earth distance as edge attribute")
+    add_earth_distnace(G)
+    print(f'Edges attributes: {list(G.edges(data=True))[0]}')
+    
+    print(f"Saving graph plot to {out_path}")
     graph_plot = pt.generate_plot(G)
     #graph_plot.show()
     graph_plot[0].savefig(out_path+'/regular_graph.png')
 
     #dataset = MyOwnDataset(root=out_path+'/regular_graph', nx_graphs=[G])
-    print(f"Comuting Floyd-Warshall distance matrix")
-    dist_mat = nx.floyd_warshall_numpy(G.to_undirected())
+    print(f"Computing Floyd-Warshall distance matrix")
+    dist_mat = nx.floyd_warshall_numpy(G.to_undirected(), weight='earth_distance')
     has_infinity = np.isinf(dist_mat).any()
     print(f"Distance matrix has infinity? {has_infinity}")
     print(f"Distance matrix is symmetric? {np.array_equal(dist_mat, dist_mat.T)}")
-
     print(f"Comuting persistence on Floyd-Warshall distance matrix")
     result = ripser(dist_mat, distance_matrix=True, maxdim=2)
     
@@ -72,6 +76,39 @@ def main(gtfs_path: str, out_path: str):
     H0_bottleneck_distance = persim.bottleneck(persistence_diagram[0], ideal_persistence_diagram[0])
     print(f"Bottleneck distance H0: {H0_bottleneck_distance}")
 
+    print(f"Checking if ideal matrix is smaller than the original matrix: {np.all(ideal_dist_mat <= dist_mat)}")
+
+    print(f"Computing speed")
+    speed = compute_speed(G)
+    print(f"Speed: {speed}")
+
+    print(f"Computing ideal graph")
+    G_ideal = compute_ideal_graph(G, speed)
+
+    print(f"Computing minimum spanning tree on ideal graph")
+    G_ideal_mst = nx.minimum_spanning_tree(G_ideal)
+
+    print(f"Computing distance matrix for the spanning tree on ideal graph")
+    dist_mat_mst = nx.floyd_warshall_numpy(G_ideal_mst.to_undirected(), weight='earth_distance')
+
+    print(f"Comuting persistence on Min Spanning Tree distance matrix")
+    ideal_mst_result= ripser(dist_mat_mst, distance_matrix=True, maxdim=1)
+    
+    print(f"Saving persistence diagrams to {out_path}")
+    ideal_mst_persistence_diagram = ideal_mst_result['dgms']
+    f, ax = plt.subplots()
+    plot_diagrams(ideal_mst_persistence_diagram, show=False)
+    f.savefig(out_path+'/persistence_diagram_mst.png')
+
+    print(f'Computing persistence statistics on the min spanning tree')
+    ideal_mst_stats = compute_persistence_statistics(ideal_mst_persistence_diagram)
+    print(ideal_mst_stats)
+
+    print(f"Computing bottleneck distance between real and mst diagrams")
+    H1_bottleneck_distance = persim.bottleneck(persistence_diagram[1], ideal_mst_persistence_diagram[1])
+    print(f"Bottleneck distance H1: {H1_bottleneck_distance}")
+    H0_bottleneck_distance = persim.bottleneck(persistence_diagram[0], ideal_mst_persistence_diagram[0])
+    print(f"Bottleneck distance H0: {H0_bottleneck_distance}")
 
 if __name__ == '__main__':
     gtfs_path = 'Data/lucerne_filtered_coords.zip'
--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ from persim import PersImage, plot_diagrams
 import networkx as nx
 from math import radians, sin, cos, sqrt, atan2
 from geopy import distance
+from networkx import set_edge_attributes
+from typing import Optional
 
 def nx_to_pyg_data(graph):
     """
@@ -109,28 +111,6 @@ def print_stats(stats):
             print(f"  {key}: {value}")
         print()
 
-
-def haversine(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great-circle distance between two points on the Earth using their latitude and longitude.
-    """
-    # Radius of the Earth in kilometers
-    R = 6371.0
-    # Radius of the Earth in meters
-    R= 6.378*1e6
-    
-    # Convert latitude and longitude from degrees to radians
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-    
-    # Differences in coordinates
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    
-    # Haversine formula
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c
-
 def compute_ideal_matrix(G: nx.Graph):
     """
     Compute the ideal distance matrix for a graph G based on Haversine distances.
@@ -152,8 +132,6 @@ def compute_ideal_matrix(G: nx.Graph):
                 # print(G.nodes[node1].keys())
                 coords_1 = (G.nodes[node1]['y'], G.nodes[node1]['x'])
                 coords_2 = (G.nodes[node2]['y'], G.nodes[node2]['x'])
-                print('{0:.15f}'.format(G.nodes[node1]['y']), G.nodes[node1]['x'])
-                
                 # Compute Haversine distance
                 distance_matrix[i][j] = distance.geodesic(coords_1, coords_2).meters
     
@@ -177,3 +155,123 @@ def normalize(matrix, method='average'):
         return matrix / np.max(matrix)
     else:
         raise ValueError("Method should be either 'average' or 'max'")
+
+def add_earth_distnace(G: nx.Graph):
+    """
+    Add an 'earth_distance' attribute to each edge in the graph G.
+    """
+    set_edge_attributes(G, None, 'earth_distance')
+
+    for u, v, key in G.edges(keys=True):
+        coords_u = (G.nodes[u]['y'], G.nodes[u]['x'])
+        coords_v = (G.nodes[v]['y'], G.nodes[v]['x'])
+        G[u][v][key]['earth_distance'] = distance.geodesic(coords_u, coords_v).meters
+
+def compute_speed(G: nx.Graph, mode: str = 'max')->float:
+    """
+    Compute the maximum speed of the edges in the graph G.
+    """
+    if not _compute_speed_check(G, mode):
+        print("length/earth_distance attributes are missing/have invalid values or mode is not valid (max/min/avg)!")
+        return float('nan')
+    
+    if len(G.edges) == 0:
+        return 0
+    
+    speeds = []
+    mode_function = _speed_mode2function(mode)
+
+    for u, v, key in G.edges(keys=True):
+        if u != v:
+            # if G[u][v][key]['length'] == 0:
+            #     print(G[u][v][key])
+            #     print(G.nodes[u])
+            #     print(G.nodes[v])
+            if G[u][v][key]['length'] != 0:
+                # Length 0 seems to be related to walking edges so they are not considered
+                speeds.append(G[u][v][key]['earth_distance'] / G[u][v][key]['length'])
+    
+    return mode_function(speeds)
+
+
+def _compute_speed_check(G: nx.Graph, mode) -> bool:
+    """
+    Compute the number of edges in the graph G that exceed the given speed limit.
+    """
+    return not (list(G.edges(data=True))[0][2].get('length') is None 
+            or list(G.edges(data=True))[0][2].get('length') == 0 
+            or list(G.edges(data=True))[0][2].get('earth_distance') is None
+            or mode not in ['max', 'min', 'avg'])
+
+def _speed_mode2function(mode: str):
+    """
+    Return the appropriate function to compute speed based on the mode.
+    """
+    if mode == 'max':
+        return np.max
+    elif mode == 'min':
+        return np.min
+    elif mode == 'avg':
+        return np.mean
+    else:
+        raise ValueError("Mode should be either 'max', 'min' or 'avg'")
+
+def compute_undirected_graph_no_multi(G: nx.Graph, mode: str = 'min') -> nx.Graph:
+    """
+    Compute the undirected version of the graph G that also stops being a multigraph.
+    """
+    G_prime = nx.Graph()
+
+    # preserve all noges and their attributes
+    for node, data in G.nodes(data=True):
+        G_prime.add_node(node, **data)
+
+    # add edges
+    for u, v in G.edges(keys=False):
+        edges = G[u,v].values()
+        
+        if mode == 'max':
+            chosen_edge = max(edges, key=lambda x: x['length'])
+        elif mode == 'min':
+            chosen_edge = min(edges, key=lambda x: x['length'])
+        elif mode == 'mean':
+            mean_value = sum(edge['length'] for edge in edges) / len(edges)
+            chosen_edge = {'length': mean_value}
+            if 'earth_distance' in G[u][v][0]:
+                mean_value = sum(edge['earth_distance'] for edge in edges) / len(edges)
+                chosen_edge['earth_distance'] = mean_value
+        else:
+            raise ValueError("Invalid mode. Choose from 'max', 'min', or 'mean'.")
+
+
+        G_prime.add_edge(u, v, **chosen_edge)
+
+    G_prime = G_prime.to_undirected()
+    return G_prime
+
+def compute_ideal_graph(G: nx.Graph, speed: Optional[float] = None) -> nx.Graph:
+    """
+    Compute the ideal graph based on Haversine distances.
+    """
+    G_prime = nx.Graph()
+
+    # preserve all nodes and their attributes
+    for node, data in G.nodes(data=True):
+        G_prime.add_node(node, **data)
+
+    # add edges
+    for u in G_prime.nodes:
+        for v in G_prime.nodes:
+            if u != v:
+                coords_u = (G.nodes[u]['y'], G.nodes[u]['x'])
+                coords_v = (G.nodes[v]['y'], G.nodes[v]['x'])
+                earth_distance = distance.geodesic(coords_u, coords_v).meters
+
+                if speed is not None:
+                    length = earth_distance / speed
+                    G_prime.add_edge(u, v, earth_distance=earth_distance, length=length)
+                else:
+                    G_prime.add_edge(u, v, earth_distance=earth_distance)
+
+    G_prime = G_prime.to_undirected()
+    return G_prime
